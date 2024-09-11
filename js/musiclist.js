651bf5e405b6330a4c9823c3b72c228269852fcc
arpita-devlops <importantworkarp@gmail.com>
2024-09-11 19:51:41 +0530

    initial comit

---
+++ js/musiclist.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "m1",
+    src: "m1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "m2",
+    src: "m2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "m3",
+    src: "m3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "m4",
+    src: "m4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "m5",
+    src: "m5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "m6",
+    src: "m6"
+  },
+];
